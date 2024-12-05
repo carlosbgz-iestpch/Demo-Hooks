869f0e7d51bec862d654726e1984bc56bd17619d
Carlos Guerrero <carlosbgz-iestpch@outlook.com>
2024-12-05 09:23:25 -0500

    Add: agregar codigo de mensaje

--- DemoHooks/Program.cs
+++ DemoHooks/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("En este proyecto se demuestra el uso de SSH en repositorios git");
+Console.WriteLine("Para mejorar la seguridad en nuestros proyectos");
